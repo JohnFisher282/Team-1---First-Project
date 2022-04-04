5e60a926201f0a8f0b1cc83b7de9b50f20e53324
Kieran Hart-Brooke <kieranhartbrooke@gmail.com>
2022-04-04 16:15:53 +0100

    added content to screens page

--- team1-finalproject/src/components/Pages/Screens.jsx
+++ team1-finalproject/src/components/Pages/Screens.jsx
@@ -1,7 +1,13 @@
 const Screens = () => {
     return (
         <>
-            <h3>Screens</h3>
+            <h3>Our Screens</h3>
+            <h4>Standard Seating</h4>
+            <img src="https://cog-home.ams3.digitaloceanspaces.com/app/uploads/2015/04/Cinema-2.jpg" alt="standardPlan"></img>
+            <img src="https://www.irishtimes.com/polopoly_fs/1.3445043.1522359625!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="standardSeats"></img>
+            <h5>Deluxe Seating</h5>
+            <img src="https://cog-home.ams3.digitaloceanspaces.com/app/uploads/2015/04/Cinema-3-Marinas-300x321.jpg" alt="deluxePlan"></img>
+            <img src="https://www.filmstories.co.uk/wp-content/uploads/2019/10/posh-seats.jpg" alt="DeluxeSeats"></img>
         </>
     );
 }
